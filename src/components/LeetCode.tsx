b5ba7227d3ba96d2f3e2f3344145c8ad54eb3116
queen <nsengaqueen123@gmail.com>
2026-04-06 22:22:29 +0200

    adding container with most water

--- src/components/LeetCode.tsx
+++ src/components/LeetCode.tsx
@@ -108,3 +108,22 @@ function threeSum(nums: number[]): number [][]{
     return result
 }
 
+
+
+
+function maxArea(height: number[]): number {
+ let left = 0;
+ let right = height.length-1;
+ let max = 0;
+ while(left<right){
+    let area = Math.min(height[left],height[right])* (right-left)
+  max = Math.max(max,area);
+  if(height[left]<height[right]){
+    left++
+  }
+  else{
+    right --
+  }
+ }
+ return max
+};
