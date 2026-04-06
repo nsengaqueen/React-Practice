31c3efac61fdde96e058697c8b95c119a1c182f2
queen <nsengaqueen123@gmail.com>
2026-04-06 11:17:10 +0200

    adding groupedAnagram

--- src/components/LeetCode.tsx
+++ src/components/LeetCode.tsx
@@ -9,3 +9,20 @@ function isAnagram(s: string, t: string): boolean {
  
     return  s.split('').sort().join('') === t.split('').sort().join('');
 };
+
+
+function groupAnagrams(strs:string[]):string[][]{
+const map = new Map <string,string[]>()
+
+for(let letter of strs){
+    const key = letter.split('').sort().join('') 
+
+    if(map.has(key)){
+        map.get(key)!.push(letter)
+    }
+    else{
+        map.set(key,[letter])
+    }
+}
+return Array.from(map.values())
+}
